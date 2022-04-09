086463bbab2ad8580d07bd0f83a5ec4d7bc7c121
Damian1457 <damianwasik.coach@gmail.com>
2022-04-09 11:09:01 +0200

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,11 +1,12 @@
 package com.kodilla.testing.collection;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OddNumbersExterminator {
     public List<Integer> exterminate(List<Integer> numbers) {
-        List<Integer> newList = new LinkedList<>();
+        List<Integer> newList = new ArrayList<>();
+
         for(Integer exterminateNew : numbers) {
             if(exterminateNew % 2 == 0) {
                 newList.add(exterminateNew);
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,21 +1,73 @@
 package com.kodilla.testing.collection;
-import org.junit.jupiter.api.*;
 
-import com.kodilla.testing.user.OddNumbersExterminator;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 
 public class CollectionTestSuite {
 
-    @Test
-    void testOddNumbersExterminatorEmptyList() {
+    private static OddNumbersExterminator oddNumbersExterminator;
 
-        //Given
+        @BeforeEach
+        public void before() {
+            System.out.println("Test case: begin");
+        }
+        @AfterEach
+        public void after() {
+            System.out.println("Test case: end");
+            }
 
+        @BeforeAll
+        public static void beforeClass() {
+            oddNumbersExterminator = new OddNumbersExterminator();
+        }
 
-        //When
+        @DisplayName
+                ("Checking if the class behaves correctly, " + "when the list is empty")
 
-        //Then
+        @Test
+        void testOddNumbersExterminatorEmptyList(){
+
+            //Given
+            ArrayList<Integer> emptyList = new ArrayList<>();
+
+            //When
+            List<Integer> newEmptyList = oddNumbersExterminator.exterminate(emptyList);
+            System.out.println("Testing empty list");
+
+            //Then
+            Assertions.assertEquals(emptyList, newEmptyList);
+        }
+
+        @DisplayName
+                ("Chcecking if the class behaves correctly, " + "when the list contains even and odd numbers")
+
+        @Test
+        void testOddNumbersExterminatorNormalList() {
+
+            //Given
+            ArrayList<Integer> newList = new ArrayList<>();
+            newList.add(1);
+            newList.add(3);
+            newList.add(5);
+            newList.add(7);
+            newList.add(10);
+            newList.add(9);
+            newList.add(12);
+
+            ArrayList<Integer> resultList = new ArrayList<>();
+            resultList.add(2);
+            resultList.add(4);
+            resultList.add(8);
+            resultList.add(10);
+
+            //When
+            List<Integer> newResultList = oddNumbersExterminator.exterminate(newList);
+            System.out.println("Testing normal List");
+            //Then
+            Assertions.assertNotEquals(resultList, newResultList);
+        }
     }
-}

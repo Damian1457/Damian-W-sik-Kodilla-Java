de9379b60aef886ceb65459c3ffce5b4812ca43d
Damian1457 <damianwasik.coach@gmail.com>
2022-04-10 22:10:54 +0200

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -6,26 +6,31 @@ import java.util.List;
 public class ShapeCollector {
 
     private List<Shape> shapeCollection = new ArrayList<>();
+
     public List<Shape> getShapeCollection() {
         return shapeCollection;
+
     }
 
     public void addFigure(Shape shape) {
-
+        shapeCollection.add(shape);
     }
 
     public boolean removeFigure(Shape shape) {
+        return shapeCollection.remove(shape);
 
-        return false;
     }
 
    public Shape getFigure(int n) {
-
-       return null;
+        if (n < shapeCollection.size() && n >= 0) {
+            return shapeCollection.get(n);
+        } else {
+            return null;
+        }
    }
 
     public void showFigures() {
-
+        System.out.println(shapeCollection.toString());
     }
 
 

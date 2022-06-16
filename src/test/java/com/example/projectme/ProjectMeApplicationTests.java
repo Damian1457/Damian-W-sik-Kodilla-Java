a5a36e9567920d66435dd76996d7e7db8fe0dcc1
Damian1457 <damianwasik.coach@gmail.com>
2022-06-16 23:08:49 +0200

    First Commit

---
+++ src/test/java/com/example/projectme/ProjectMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

52e44243a2db251e277eba172290e322883e9088
Damian1457 <damianwasik.coach@gmail.com>
2022-07-18 22:41:03 +0200

    Creation of a product ordering website

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/order/InformationOrderService.java
@@ -0,0 +1,7 @@
+package com.kodilla.good.patterns.challenges.order;
+
+public interface InformationOrderService {
+
+    void emailInformation(User user, Product product);
+
+}
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/order/User.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/order/User.java
@@ -3,11 +3,17 @@ package com.kodilla.good.patterns.challenges.order;
 public class User {
 
     private String userName;
+    private String adres;
     private String email;
 
-    public User(String userName, String email) {
+    public User(String userName, String email, String adres) {
         this.userName = userName;
         this.email = email;
+        this.adres = adres;
+    }
+
+    public String getAdres() {
+        return adres;
     }
 
     public String getUserName() {
